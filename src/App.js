86cccc3aad869f221863f5441ca27b4491a900ee
jerryfishzz <jerryfish@163.com>
2020-05-04 14:12:17 +1200

    Initialize project
    feat: Use workspace settings to initialize the local enviroment
    
    - Set the API call in App.js to get data; modify file content
    
    - Modify CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [backend, setBackend] = useState('backend-data');
+
+  useEffect(() => { 
+    const api = process.env.REACT_APP_BACKEND ||  'http://localhost:3001';
+    const url = `${api}/categories`;
+    console.log('fetching from url', url);
+    
+    fetch(url, { headers: { 'Authorization': 'whatever-you-want' }} )
+      .then( (res) => { return(res.text()) })
+      .then((data) => {
+        setBackend(data);
+      });
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Welcome to React</h2>
+      </div>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <p>
+        Talking to the backend yields these categories: <br />
+        {backend}
+      </p>
     </div>
   );
 }
