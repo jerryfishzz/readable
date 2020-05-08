c0666ddb1f98f2c46d6afd3871457a692aff9797
jerryfishzz <jerryfish@163.com>
2020-05-09 11:20:41 +1200

    Material UI
    feat: Install Material UI and do some initial settings

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import logo from '../logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 import { handleGetCategories } from '../actions/categories';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App(props) {
   useEffect(() => { 
@@ -17,19 +18,22 @@ function App(props) {
   const backend = JSON.stringify(props.categories)
 
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Fragment>
+      <CssBaseline />
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Talking to the backend yields these categories: <br />
+          {backend}
+        </p>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-      <p>
-        Talking to the backend yields these categories: <br />
-        {backend}
-      </p>
-    </div>
+    </Fragment>
   );
 }
 
